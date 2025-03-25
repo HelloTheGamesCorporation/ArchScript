1621cfba6dc8d46b92518929880153d11467e0d8
ArchLinuxUser <85604075+HelloTheGamesCorporation@users.noreply.github.com>
2025-03-25 18:02:52 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ echo "vda"
 echo "nvme0n1"
 echo "sda"
 read -p "Choose a disk(Enter in words): " answer
-sleep 5
+sleep 2
 echo "How do you want to partition disk?"
 echo "1)Auto(Use a read-made option(for virtual 20GB is required, and for hardware 512GB)"
 echo "2)Manually(via cfdisk)"
